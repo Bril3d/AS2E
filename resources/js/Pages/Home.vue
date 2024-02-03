415e2f31e1db20392a19d0abcc51946022d9b9ea
Bril3d <khaledtbk@gmail.com>
2024-02-03 16:13:05 +0100

    Adding Preline UI

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import Hero from '../Components/Hero.vue';
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <Hero />
+  
+</template>
+
