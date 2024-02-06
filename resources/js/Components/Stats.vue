665d3c3cfc75b62c226c467d43deccbbf7d1dbbc
Bril3d <khaledtbk@gmail.com>
2024-02-06 03:02:44 +0100

    Stats Section

---
+++ resources/js/Components/Stats.vue
@@ -0,0 +1,42 @@
+<template lang="">
+<!-- Features -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <SectionHeader title="Project Stats" caption="Key metrics for our software development project" />
+  <!-- Grid -->
+  <div class="mt-20 grid gap-6 grid-cols-2 sm:gap-12 lg:grid-cols-3 lg:gap-8 text-center">
+    <!-- Stats -->
+    <div>
+      <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Accuracy rate</h4>
+      <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">99.95%</p>
+      <p class="mt-1 text-gray-500">in fulfilling orders</p>
+    </div>
+    <!-- End Stats -->
+
+    <!-- Stats -->
+    <div>
+      <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Team Members</h4>
+      <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">50+</p>
+      <p class="mt-1 text-gray-500">dedicated team members</p>
+    </div>
+    <!-- End Stats -->
+
+    <!-- Stats -->
+    <div>
+      <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Happy customer</h4>
+      <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">85%</p>
+      <p class="mt-1 text-gray-500">this year alone</p>
+    </div>
+    <!-- End Stats -->
+  </div>
+  <!-- End Grid -->
+</div>
+<!-- End Features -->
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+  
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,6 +5,7 @@ import Features from '@/Components/Features.vue';
 import Services from '@/Components/Services.vue'
 import Faq from '@/Components/Faq.vue';
 import Footer from '@/Components/Footer.vue'
+import Stats from '@/Components/Stats.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import Footer from '@/Components/Footer.vue'
         <Hero />
         <Features />
         <Services />
+        <Stats />
         <Faq />
         <Footer />
     </div>
