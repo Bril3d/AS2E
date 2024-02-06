bca1737b4ae2f2afa6f4c6a03f508c36196f0034
Bril3d <khaledtbk@gmail.com>
2024-02-06 03:09:38 +0100

    Announcement Banner

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
+import AnnouncementBanner from '@/Components/AnnouncementBanner.vue';
 import Hero from '../Components/Hero.vue';
 import Features from '@/Components/Features.vue';
 import Services from '@/Components/Services.vue'
@@ -11,6 +12,7 @@ import Stats from '@/Components/Stats.vue'
 <template>
     <Head title="Welcome" />
     <div>
+        <AnnouncementBanner content="This website still under development." />
         <Hero />
         <Features />
         <Services />
