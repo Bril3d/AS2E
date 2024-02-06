4d26c056170cfdba02cc3d5b6b2ed5a5138f8120
Bril3d <khaledtbk@gmail.com>
2024-02-06 01:44:15 +0100

    Features Section

---
+++ resources/js/Components/Features.vue
@@ -0,0 +1,91 @@
+<template>
+  <!-- Features -->
+  <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <!-- Grid -->
+    <div class="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
+      <div>
+        <img class="rounded-xl"
+          src="https://images.unsplash.com/photo-1648737963503-1a26da876aca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&h=900&q=80"
+          alt="Image Description">
+      </div>
+      <!-- End Col -->
+
+      <div class="mt-5 sm:mt-10 lg:mt-0">
+        <div class="space-y-6 sm:space-y-8">
+          <!-- Title -->
+          <div class="space-y-2 md:space-y-4">
+            <h2 class="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
+              Advanced Features Driving Efficiency: Elevating Automation Systems and Energy Performance
+            </h2>
+            <p class="text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nobis commodi, dolorum similique quisquam
+              eius itaque repellat maxime, sequi suscipit ratione explicabo vitae harum cum ea! Alias, dolor vero!
+              Deserunt!
+            </p>
+          </div>
+          <!-- End Title -->
+
+          <!-- List -->
+          <ul role="list" class="space-y-2 sm:space-y-4">
+            <li class="flex space-x-3">
+              <span
+                class="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                <svg class="flex-shrink-0 h-3.5 w-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round">
+                  <polyline points="20 6 9 17 4 12" />
+                </svg>
+              </span>
+
+              <span class="text-sm sm:text-base text-gray-500">
+                <span class="font-bold">Automated Control </span> Systems
+              </span>
+            </li>
+
+            <li class="flex space-x-3">
+              <span
+                class="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                <svg class="flex-shrink-0 h-3.5 w-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round">
+                  <polyline points="20 6 9 17 4 12" />
+                </svg>
+              </span>
+
+              <span class="text-sm sm:text-base text-gray-500">
+                Energy Management and <span class="font-bold">Monitoring</span>
+              </span>
+            </li>
+
+            <li class="flex space-x-3">
+              <span
+                class="mt-0.5 h-5 w-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+                <svg class="flex-shrink-0 h-3.5 w-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round">
+                  <polyline points="20 6 9 17 4 12" />
+                </svg>
+              </span>
+
+              <span class="text-sm sm:text-base text-gray-500">
+                Predictive Maintenance with Condition Monitoring
+              </span>
+            </li>
+          </ul>
+          <!-- End List -->
+        </div>
+      </div>
+      <!-- End Col -->
+    </div>
+    <!-- End Grid -->
+  </div>
+  <!-- End Features -->
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+  
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import Hero from '../Components/Hero.vue';
+import Features from '@/Components/Features.vue';
 </script>
 
 <template>
     <Head title="Welcome" />
-
-    <Hero />
-  
+    <div>
+        <Hero />
+        <Features />
+    </div>
 </template>
 
