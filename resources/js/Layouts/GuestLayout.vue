cf6c6a3955bd5cd2922983303576c948d78346c2
Bril3d <khaledtbk@gmail.com>
2024-03-19 15:27:20 +0100

    Sign in page dark mode

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex sm:flex-col sm:justify-center items-center py-6 bg-gray-100">
+    <div class="min-h-screen flex sm:flex-col sm:justify-center items-center py-6 dark:bg-slate-900 bg-gray-100">
         <div class="hidden sm:block">
             <Link href="/">
                 <ApplicationLogo class="w-36 sm:w-64" />
@@ -12,7 +12,7 @@ import { Link } from '@inertiajs/vue3';
         </div>
 
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 shadow-md overflow-hidden sm:rounded-lg"
         >
             <slot />
         </div>
