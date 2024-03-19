09adcb4946961183d8275c75b5f45d2b25fb9b71
Bril3d <khaledtbk@gmail.com>
2024-03-19 15:41:43 +0100

    Sign Up Page Dark Mode

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -6,7 +6,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
-import { CaUserAvatar } from "@kalimahapps/vue-icons";
+import { CaUserAvatar, DeGoogleOriginal } from "@kalimahapps/vue-icons";
 
 const url = ref(null);
 
@@ -34,80 +34,79 @@ const submit = () => {
     <GuestLayout>
 
         <Head title="Register" />
-        
-        <a href="/auth/google/redirect"
-            class="w-full cursor-pointer py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-            <svg class="w-4 h-auto" width="46" height="47" viewBox="0 0 46 47" fill="none">
-                <path
-                    d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
-                    fill="#4285F4" />
-                <path
-                    d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
-                    fill="#34A853" />
-                <path
-                    d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
-                    fill="#FBBC05" />
-                <path
-                    d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
-                    fill="#EB4335" />
-            </svg>
-            Sign in with Google
-        </a>
 
-        <div
-            class="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
-            Or</div>
+        <div class="text-center">
+            <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign up</h1>
+            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                Already have an account?
+                <Link
+                    class="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    :href="route('login')">
+                Sign in here
+                </Link>
+            </p>
+        </div>
+        <div class="mt-5">
+            <a href="/auth/google/redirect"
+                class="w-full cursor-pointer py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                <DeGoogleOriginal class="w-4 h-auto" />
+                Sign up with Google
+            </a>
+
+            <div
+                class="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
+                Or</div>
+        </div>
         <form @submit.prevent="submit">
             <div>
                 <input type="file" @input="onFileChange" class="hidden" ref="file" />
-                <div class="h-32 w-32 rounded-full bg-slate-600 my-4 mx-auto flex justify-center items-center"
+                <div class="h-32 w-32 rounded-full bg-slate-600 my-4 mx-auto flex justify-center items-center cursor-pointer"
                     @click="$refs.file.click()">
                     <img v-if="url" :src="url" class="w-full h-full rounded-full object-cover" alt="Avatar Preview">
-                    <CaUserAvatar v-else class="fill-white w-24 h-24" />
+                    <CaUserAvatar v-else class="fill-white w-24 h-24 hover:opacity-50 transition-opacity ease-in duration-200" />
                 </div>
                 <p class="text-center font-bold text-sm text-gray-400">{{ form.avatar?.name || 'Import an avatar' }}</p>
 
-                <InputLabel for="name" value="Name" />
+                <InputLabel for="name" value="Name" class="dark:text-white" />
 
-                <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus
-                    autocomplete="name" />
+                <TextInput id="name" type="text"
+                    class="mt-1 block w-full dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                    v-model="form.name" required autofocus autocomplete="name" />
 
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email" value="Email" class="dark:text-white" />
 
-                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
-                    autocomplete="username" />
+                <TextInput id="email" type="email"
+                    class="mt-1 block w-full dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                    v-model="form.email" required autocomplete="username" />
 
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" value="Password" class="dark:text-white" />
 
-                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-                    autocomplete="new-password" />
+                <TextInput id="password" type="password"
+                    class="mt-1 block w-full dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                    v-model="form.password" required autocomplete="new-password" />
 
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
+                <InputLabel for="password_confirmation" value="Confirm Password" class="dark:text-white" />
 
-                <TextInput id="password_confirmation" type="password" class="mt-1 block w-full"
+                <TextInput id="password_confirmation" type="password"
+                    class="mt-1 block w-full dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
                     v-model="form.password_confirmation" required autocomplete="new-password" />
 
                 <InputError class="mt-2" :message="form.errors.password_confirmation" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Already registered?
-                </Link>
-
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Register
                 </PrimaryButton>
