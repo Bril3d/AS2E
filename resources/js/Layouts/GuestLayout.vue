f89a1268aa7532d6128250914d511e46ba5365f8
Bril3d <khaledtbk@gmail.com>
2024-03-25 04:02:46 +0100

    Global css route transition

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -15,28 +15,3 @@ import { Link } from '@inertiajs/vue3';
 </template>
 
 
-<style>
-.route-enter-from {
-    opacity: 0;
-    transform: translateY(-30px);
-}
-
-.route-leave-to {
-    opacity: 0;
-    transform: translateY(-10px);
-}
-
-.route-enter-active {
-    transition: all 0.5s ease-out;
-}
-
-.route-leave-active {
-    transition: all 0.3s ease-in;
-}
-
-.route-enter-to,
-.route-leave-from {
-    opacity: 1;
-    transform: translateY(0);
-}
-</style>
