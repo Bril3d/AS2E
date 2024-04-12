c0ff3f6e99fdeb34f2673c3bd818af43a00791aa
Bril3d <khaledtbk@gmail.com>
2024-04-12 20:24:24 +0100

    App logo fallback

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -2,10 +2,12 @@
 import { usePage } from '@inertiajs/vue3';
 
 let { logo, name } = usePage().props.settings;
-logo = '/storage/' + logo
+logo = logo ? '/storage/' + logo : null
+name = name ? name : 'AS2E'
 </script>
 
 
 <template>
-    <img :src="logo" :alt="name + ' LOGO'">
+    <img v-if="logo" :src="logo" :alt="name + ' LOGO'">
+    <h1 class="text-2xl uppercase" v-else>{{ name }}</h1>
 </template>
