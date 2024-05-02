2e6ee39d3982964253c40df9cb4943b70f64c981
Bril3d <khaledtbk@gmail.com>
2024-05-02 20:01:51 +0100

    Back button in post view

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -1,6 +1,8 @@
 <template>
+
   <Head title="Post" />
   <AuthenticatedLayout>
+    <SecondaryButton class="mb-4" @click="goBack">Back</SecondaryButton>
     <PostCard :post="post" />
   </AuthenticatedLayout>
 </template>
@@ -8,8 +10,15 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import PostCard from '@/Components/PostCard.vue'
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+
 
 const props = defineProps({
   post: Object
 })
+
+const goBack = () => {
+  window.history.back();
+}
+
 </script>
