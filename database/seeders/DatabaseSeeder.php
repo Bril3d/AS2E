9121b0b1594df2e0f4b44857a040a30934ab47e0
Bril3d <khaledtbk@gmail.com>
2024-05-13 19:01:26 +0100

    Permission Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        
+        $this->call(PermissionSeeder::class);
 
         $this->call(RoleSeeder::class);
 
---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = ['Dashboard View', 'Users CRUD', 'Roles CRUD', 'Appointments CRUD', 'Settings CRUD', 'Projects CRUD', 'Posts CRUD', 'Comments CRUD', 'Expertises CRUD', 'File Explorer CRUD'];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
